091d0668b68d3da293e4be20ae939ab9693e097e
Andydemarks <andydemarks@andydemarks.com>
2022-01-19 15:22:39 -0500

    Added code for console menu

--- C#/HelloWorld/Car.cs
+++ C#/HelloWorld/Car.cs
@@ -3,6 +3,57 @@ namespace CarFunction
     //Car class
     public class Car
     {
+        //Field: stores information or defines current state of object
+        //private means only the class itself can use the field
+        private string _color;
+        private string _owner;
+        private int _gallonPerMile;
+
+        //Property
+        //They are in PascalCase
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public string Owner
+        {
+            get { return _owner; }
+            set { _owner = value; }
+        }
         
-    }   
+        public int Fuel { get; set; }
+
+        //basic method
+        public void Start()
+        {
+            Console.WriteLine("The car is starting now!");
+            Console.WriteLine($"Current Fuel: {Fuel}");
+        }
+
+        public void Start(int p_fuel)
+        {
+            Fuel = p_fuel;
+            Console.WriteLine("The car is starting now!");
+            Console.WriteLine($"Current Fuel: {Fuel}");
+
+        }
+
+        public Car()
+        {
+            _color = "Blue";
+            _owner = "No Owner";
+            _gallonPerMile = 10;
+            Fuel = 100;    
+        }
+
+        //will give the total distance of the car
+        public int TotalDistance()
+        {
+            return Fuel/_gallonPerMile;
+        }
+
+    }
+
 }
---
+++ C#/HelloWorld/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+// using CarFunction;
+
+// Console.WriteLine("Hello, World!");
+
+// //NameofClass NameofVariable = new NameofClass();
+// Car car1 = new Car();
+// Console.WriteLine(car1.Color);
+// car1.Color = "Red";
+// Console.WriteLine(car1.Color);
+// car1.Fuel = 10;
+// Console.WriteLine(car1.Fuel);
+// car1.Start();
+// car1.Start(100);
+
+// Car car2 = new Car()
+// {
+//     Color = "Yellow",
+//     Fuel = 100,
+//     Owner = "Stephen"
+// };
+
+// Console.WriteLine(car2.Color);
+//Console.WriteLine(car2.TotalDistance());
+
+// Console.WriteLine("Welcome to programming!");
+// Console.WriteLine("Please tell me your name:");
+// string userName = Console.ReadLine();
+// Console.WriteLine($"Hello {userName}, what do you want to do today?");
+// Console.WriteLine("1: Add 2 Numbers");
+// Console.WriteLine("2: Exit Program");
+
+// string userInput = Console.ReadLine();
+
+// if (userInput == "1")
+// {
+//     Console.WriteLine("Please give me two numbers:");
+//     int num1 = Convert.ToInt32(Console.ReadLine());
+//     int num2 = Convert.ToInt32(Console.ReadLine());
+
+//     Console.WriteLine($"The sum is: {num1+num2}");    
+// }
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool repeat = true;
+int skydives = 0;
+int scubaDiveTime = 0;
+
+
+while (repeat)
+{
+    Console.WriteLine("-----------------------------------------------");
+    Console.WriteLine("Andrew's Adventure Menu");
+    Console.WriteLine("Enter a number for what you would like to do:");
+    
+    Console.WriteLine("1: Go Skydiving!");
+    Console.WriteLine("2: Go Scuba Diving!");
+    Console.WriteLine("3: Go to an Amusement Park");
+    Console.WriteLine("4: Go to Bed (this will exit program)");
+
+    string menuInput = Console.ReadLine();
+
+    switch(menuInput)
+    {
+        case "1":
+            skydives = skydives + 1;
+            Console.WriteLine($"Congradulations! You have gone skydiving {skydives} time(s)!");
+            break;
+        case "2":
+            Console.WriteLine("How many minutes did you scuba dive for?");
+            int scubaTime = Convert.ToInt32(Console.ReadLine());
+            scubaDiveTime = scubaDiveTime + scubaTime;
+            Console.WriteLine($"Nice! That makes {scubaDiveTime} minute(s) in total!");
+            break;
+        case "3":
+            Console.WriteLine("What did you ride?");
+            string amusementRide = Console.ReadLine();
+            Console.WriteLine($"{amusementRide} is a great ride!");
+            break;
+        case "4":
+            Console.WriteLine("Goodnight.");
+            repeat = false;
+            break;
+    }
+
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
